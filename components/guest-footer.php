2c414dd02cff6a25abf46583be577faa947bc1b4
aura-codez <muqadasmeherban@gmail.com>
2025-03-05 21:47:48 +0500

    Update instagram link

--- components/guest-footer.php
+++ components/guest-footer.php
@@ -1,6 +1,6 @@
 <footer class="text-center text-light p-3" style="background-color: #111;">
     <p>Follow us: 
-        <a href="https://www.instagram.com/must_university-officially" class="text-decoration-none text-warning">Instagram</a> | 
+        <a href="https://www.instagram.com/must_university_officially?igsh=MWp0ZX5dHFuZWox" class="text-decoration-none text-warning">Instagram</a> | 
         <a href="https://www.facebook.com/profile.php?id=100079274237254" class="text-decoration-none text-warning">Facebook</a> | 
         <a href="https://www.must.edu.pk" class="text-decoration-none text-warning">Official Website</a>
     </p>

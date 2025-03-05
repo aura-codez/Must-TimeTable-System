9410b427749a3bc175addabf0718a19c99833766
aura-codez <muqadasmeherban@gmail.com>
2025-03-06 00:42:07 +0500

    Update teacher-header.php
    added cdn link of js

--- components/teacher-header.php
+++ components/teacher-header.php
@@ -17,3 +17,4 @@
     </div>
 </nav>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>

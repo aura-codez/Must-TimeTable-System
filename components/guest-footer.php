2ed6ff206238a5222350ac03422bb7f9ebf029da
aura-codez <muqadasmeherban@gmail.com>
2025-03-05 21:52:43 +0500

    changes link's color

--- components/guest-footer.php
+++ components/guest-footer.php
@@ -1,8 +1,8 @@
 <footer class="text-center text-light p-3" style="background-color: #111;">
     <p>Follow us: 
-        <a href="https://www.instagram.com/must_university_officially?igsh=MWp0ZX5dHFuZWox" class="text-decoration-none text-warning">Instagram</a> | 
-        <a href="https://www.facebook.com/profile.php?id=100079274237254" class="text-decoration-none text-warning">Facebook</a> | 
-        <a href="https://www.must.edu.pk" class="text-decoration-none text-warning">Official Website</a>
+        <a href="https://www.instagram.com/must_university_officially?igsh=MWp0ZX5dHFuZWox" class="text-decoration-none .btn-instagram">Instagram</a> | 
+        <a href="https://www.facebook.com/profile.php?id=100079274237254" class="text-decoration-none .btn-facebook">Facebook</a> | 
+        <a href="https://www.must.edu.pk" class="text-decoration-none .btn-social">Official Website</a>
     </p>
     <p>Created by Muqadas Meherban & Ahmed Ali (Session 22-26, Mirpur University of Science and Technology).</p>
 </footer>
